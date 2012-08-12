cb517097c83e8c00f2b60c720953f81b9bf4bf42
Eric Marden <eric@xentek.net>
2012-08-12 04:13:26 -0500

    fixes bugs with sorting

--- public/js/shomei.js
+++ public/js/shomei.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  $('tbody').load('/pings');
+  $('tbody').load('/pings?col=updated_at&sort=desc');
 
   $('th').live('click', function() {
-    var sort = $(this).data('sort') || 'asc';
+    var sort = $(this).data('sort') || 'desc';
     var new_sort = (sort == 'asc') ? 'desc' : 'asc';
     var col = $(this).data('col');
-    var req = '/pings?col='+col+'&sort='+sort;
+    var req = '/pings?col='+col+'&sort='+new_sort;
+    $('tbody').load(req);      
     $(this).data('sort', new_sort);
     $('th').not(this).removeAttr('data-sort');
     $('th').not(this).removeClass(sort).removeClass(new_sort);
     $(this).removeClass(sort).addClass(new_sort);
-    $('tbody').load(req);    
   });
 });
