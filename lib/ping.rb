dd19542630ce1f31687faae72ede0880c705671b
Eric Marden <eric@xentek.net>
2012-08-13 05:03:13 -0500

    adds method to sort out the button css class

--- lib/ping.rb
+++ lib/ping.rb
@@ -8,4 +8,14 @@ class Ping
   property :updated_at, DateTime
 
   validates_presence_of :machine, :status
+
+  def button_css_class
+    if status == 'failure'
+      'alert'
+    elsif status == 'success' and updated_at > (Time.now - (60*60))
+      'success'
+    else 
+     'secondary'
+    end
+  end
 end
